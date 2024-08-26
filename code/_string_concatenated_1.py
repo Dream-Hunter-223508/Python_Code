4060cc059a7e55edbf48de2b3bfb86adc756c2e7
Murad734138 <157712320+Dream-Hunter-223508@users.noreply.github.com>
2024-08-26 15:32:56 +0600

    string concatenated

---
+++ code/_string_concatenated_1.py
@@ -0,0 +1,21 @@
+
+
+def endl():
+    print("\n")
+
+
+def spes():
+    print(" ")
+
+
+#--> 3 times 'un', followed by 'ium'..
+endl()
+print(3*"un"+"ium")
+
+print('Py''thon')
+print('Py'+'thon')
+print('Py','thon') #--> coma creat a extra space..
+
+ami="Muraduzzaman"
+print(ami+" Asha")
+print(ami,"Asha")
