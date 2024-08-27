44539bc93b1ae1ff7d6ad218807c42b51006719a
Murad734138 <157712320+Dream-Hunter-223508@users.noreply.github.com>
2024-08-27 12:48:24 +0600

    string modify

---
+++ code/_string_modify_1.py
@@ -0,0 +1,29 @@
+
+
+def endl():
+    print("\n")
+
+
+def spes():
+    print(" ")
+
+s1=input() #---> python user input string.
+
+print(s1)
+endl()
+
+print("Show lenth of string :",len(s1))
+s2="Python"
+print("Show lenth of string :",len(s2))
+print(s1[4:])
+print(s2[-5:])
+endl()
+
+print("Negative & positive possion of string:")
+print("""
+     +---+---+---+---+---+---+
+     | P | y | t | h | o | n |
+     +---+---+---+---+---+---+
+     0   1   2   3   4   5   6-
+     6  -5  -4  -3  -2  -1
+             """ )
